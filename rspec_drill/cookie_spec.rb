e75acbf27f3ba09fa69bac59587e883ab9e10ec3
bestofthesoul <bestofthesoul@gmail.com>
2016-10-26 18:44:37 +0800

    Update cookie_spec.rb

--- rspec_drill/cookie_spec.rb
+++ rspec_drill/cookie_spec.rb
@@ -1,94 +1,84 @@
+
 require 'rspec'
 
 require_relative 'cookie'
 
 describe Cookie do
-# general
+
   let(:type)   { "peanut butter" }
+  #abc = "peanut butter"
+
   let(:cookie) { Cookie.new(type) }
+  #cookie = Cookie.new()
 
   describe "#initialize" do
-    context "with valid input" do
-      it "creates a new Cookie correctly according to what i had specified on the type the specified type" do
-        # https://www.relishapp.com/rspec/rspec-expectations/v/2-13/docs/built-in-matchers/equality-matchers
-        # WRITE YOUR CODE HERE
+    context "with valid input" do #happy path
+      it "creates a new Cookie of the specified type" do
+        # http://www.relishapp.com/rspec/rspec-expectations/v/3-5/docs/built-in-matchers
+
 
       end
     end
 
-    context "with invalid input" do
+    context "with invalid input" do #unhappy path
       it "throws an argument error when not given a type argument" do
-        # https://www.relishapp.com/rspec/rspec-expectations/v/2-0/docs/matchers/expect-error
-         # WRITE YOUR CODE HERE
+         # http://www.relishapp.com/rspec/rspec-expectations/v/3-5/docs/built-in-matchers/raise-error-matcher
+
+ 
+
       end
     end
   end
 
   describe "#type" do
     it "returns the type of the cookie" do
-       # WRITE YOUR CODE HERE
+
     end
   end
 
   describe "#bake!" do
+    # https://www.relishapp.com/rspec/rspec-expectations/v/2-0/docs/matchers/type-check-matchers
     it "requires an integer time argument" do
-      # https://www.relishapp.com/rspec/rspec-expectations/v/2-0/docs/matchers/type-check-matchers
 
-      # WRITE YOUR CODE HERE
     end
 
-    it "returns the cookie object" do
-      # https://www.relishapp.com/rspec/rspec-expectations/v/3-3/docs/built-in-matchers/equality-matchers
-       # WRITE YOUR CODE HERE
-    end
 
-    it "changes the status of the cookie when given enough time" do
-       # WRITE YOUR CODE HERE
-    end
+  # #   it "returns the cookie object"
+
+
+  #   it "changes the status of the cookie when given enough time" do
+  #     # https://www.relishapp.com/rspec/rspec-expectations/docs/built-in-matchers
+
+  #   end
   end
 
-  describe "#status" do
-    it "returns the cookie's current status" do
-       # WRITE YOUR CODE HERE
-    end
+  # describe "#status" do
+  #   it "returns the cookie's current status"
 
-    context "when unbaked" do
-      it "is `:doughy`" do
-       expect {cookie.bake!(7)}.to change(cookie, :status)
-      end
-    end
 
-    context "when baked for less than 7 minutes" do
-      it "is `:doughy`" do
-        # cookie.bake!(6)
-        expect(cookie.bake!(6).status).to eq(:doughy)
-      end
-    end
+  #   context "when unbaked" do
+  #     it "is `:doughy`"
 
-    context "when baked for at least 7 but less than 10 minutes" do
-      it "is `:almost_ready`" do
-         # WRITE YOUR CODE HERE
-      end
-    end
+  #   end
 
-    context "when baked for at least 10 but less than 12 minutes" do
-      it "is `:ready`" do
-         # WRITE YOUR CODE HERE
-      end
-    end
+  #   context "when baked for less than 7 minutes" do
+  #     it "is `:doughy`"
 
-    context "when baked for at least 12 minutes" do
-      it "is `:burned`" do
-         # WRITE YOUR CODE HERE
-      end
-    end
+  #   end
 
-  end
+  #   context "when baked for at least 7 but less than 10 minutes" do
+  #     it "is `:almost_ready`"
+
+  #   end
+
+  #   context "when baked for at least 10 but less than 12 minutes" do
+  #     it "is `:ready`"
+
+  #   end
+
+  #   context "when baked for at least 12 minutes" do
+  #     it "is `:burned`"
+
+  #   end
+  # end
 end
-
-
-
-
-#     end
-#   end
-# end
